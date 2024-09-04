1b22feca266140d25f94bc0ebcb130212ec94ce3
Droxus <alexredov@gmail.com>
2024-09-04 12:54:43 +0100

    form added

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello");
+console.log("The script is executing");
+
+document.querySelector("#submitBtn").addEventListener("click", submitForm);
+
+function getFormData() {
+  const formData = {
+    name: document.querySelector("#name").value,
+    email: document.querySelector("#email").value,
+    message: document.querySelector("#message").value,
+  };
+
+  return formData;
+}
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const formData = getFormData();
+  console.log(formData);
+}
